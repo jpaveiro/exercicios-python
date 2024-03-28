2ace87d24757b604da39997dcdd1b724b94a3998
João Pedro Aveiro <129242309+jpaveiro@users.noreply.github.com>
2024-03-28 17:27:36 -0300

    Update listade11.py

--- listade11.py
+++ listade11.py
@@ -1,160 +1,61 @@
 import os
+from funcao import Funcao
 
-while opcao != 0:
+continuar = True
+
+while (continuar):
     os.system("cls")
+    opcao = int(input("Digite o número do exercício que deseja executar (1 até 11)\n=> "))
 
-    print("""
-    0 - para sair
-    exercicio 1 - 2 valores
-    exercicio 2 - Ano de nascimento
-    exercicio 3 - Validade senha
-    exercicio 4 - Valor total maças
-    exercicio 5 - 3 valores inteiros
-    exercicio 6 - Peso ideal
-    exercicio 7 e 8 - Poligno regular
-    exercicio 9 - 3 valores inteiros, exibir maior
-    exercicio 10 - Triangulo Equilatero, Isosceles ou Escaleno
-    exercicio 11 - Triangulo Acutangulo, Retangulo, Obtusangulo
-    """)
-
-    def twoValues():
-        n1 = float(input("Digite o 1 numero: "))
-        n2 = float(input("Digite o 2 numero: "))
-        if n1 > n2:
-            print(f"O numero maior e {n1}")
-        elif n1<n2:
-            print(f"O numero maior e {n2}")
-        else:
-            print("Nao tem maior")
-
-    def birthyearVote():
-        birthyear = int(input("Qual seu ano de nascimento? "))
-        if birthyear > 2006:
-            print("Nao pode votar")
-        else:
-            print("Pode votar")
-
-    def passwordVerify():
-        password = int(input("Digite a senha: "))
-        if password == 1234:
-            print("Acesso permitido")
-        else:
-            print("Acesso negado")
-
-    def appleValue():
-        appleBuyed = float(input("Quantas maças foram compradas? "))
-
-        if appleBuyed < 12:
-            vt = appleBuyed * 0.3
-            print(f"O valor total da compra e: {vt}")
-        else:
-            vt = appleBuyed * 0.25
-            print(f"O valor total da compra e: {vt}")
-
-    def threeValuesCrescent():
-        value1 = int(input("Digite um valor:"))
-        value2 = int(input("Digite outro valor:"))
-        value3 = int(input("Digite mais um valor:"))
-        ordCres = [value1,value2,value3] 
-        ordCres.sort()
-        print(f"A ordem crescente e {ordCres}")
-
-    def idealWeight():
-        height = float(input("Qual sua altura:"))
-        sex = int(input("Você é - 1:feminino 2:masculino? "))
-    
-        if sex == 1:
-            weightIdeal = (62.1 * height) - 44.7
-            print(f"O seu peso ideal é {weightIdeal:.2f}")
-        elif sex == 2:
-            weightIdeal = (72.7 * height) - 58
-            print(f"O seu peso ideal é {weightIdeal:.2f}")
-        else:
-            print("Eu simplesmente nao existo, ha apenas uma ideia de patrick bateman")
-
-    def regularPolygon():
-        numberSide = int(input("Digite o numero de lados:"))
-        sidecm = float(input("Quantos centimetros tem um lado do poligono? "))
-
-        if numberSide < 3:
-            print("Nao e um poligono")
-        elif numberSide == 3:
-            area = sidecm*sidecm*1.7/4.0
-            print(f"TRIANGULO {area:.2f}")
-        elif numberSide == 4:
-            area = sidecm*sidecm
-            print(f"QUADRADO {area:.2f}")
-        elif numberSide == 5:
-            print(f"PENTAGONO")
-        else:
-            print("Poligono nao identificado")
-
-    def threeValuesBiggest():
-        value1 = int(input("Digite um valor:"))
-        value2 = int(input("Digite outro valor:"))
-        value3 = int(input("Digite mais um valor:"))
-
-        if value1 > value2 and value1 > value3:
-            biggestValue = value1
-        elif value2 > value1 and value2 > value3:
-            biggestValue = value2
-        else:
-            biggestValue = value3
-        print(f"O maior valor é {biggestValue}")
-
-    def trianguleDefinition():
-        sidecm1 = float(input("Quantos centimetros tem um lado do triangulo? "))
-        sidecm2 = float(input("Quantos centimetros tem o outro lado do triangulo? "))
-        sidecm3 = float(input("Quantos centimetros tem o ultimo lado do triangulo? "))
-
-        if sidecm1 == sidecm2 and sidecm1 == sidecm3:
-            print("Triangulo equilatero possui 3 lados iguais")
-        elif sidecm1 == sidecm2 or sidecm1 == sidecm3:
-            print("Triangulo Isosceles possui 2 lados iguais")
-        else:
-            print("Triangulo escaleno possui 3 lados diferentes")
-
-    def trianguleAngule():
-        angule1 = float(input("Qual o 1 angulo? "))
-        angule2 = float(input("Qual o 2 angulo? "))
-        angule3 = float(input("Qual o 3 angulo? "))
-
-        if angule1 == 90 or angule2 == 90 or angule3 == 90:
-            print("Triangulo Retangulo")
-        elif angule1 >= 90 or angule2 >= 90 or angule3 >= 90:
-            print("Triangulo Obtusangulo")
-        else:
-            print("Triangulo Acutangulo")
-
-    opcao = int(input("Digite um exercicio:"))
-
-    match opcao:
+    match(opcao):
         case 0:
-            print("Finalizando execução")
-            break
+            print("Saindo do programa. Obrigado por testar!")
+            continuar = False
         case 1:
-            twoValues()
+            n1 = int(input("Digite um número:\n=> "))
+            n2 = int(input("Digite outro número:\n=> "))
+            Funcao.comparacao(n1, n2)
         case 2:
-            birthyearVote()
+            anoNascimento = int(input("Digite um ano:\n=> "))
+            Funcao.podeVotar(anoNascimento)
         case 3:
-            passwordVerify()
+            senha = str(input("Digite a senha:"))
+            Funcao.senhaCorreta(senha)
         case 4:
-            appleValue()
+            nmrMacas = int(input("Digite a quantidade de maçãs que deseja comprar:\n=> "))
+            Funcao.comprarMaca(nmrMacas)
         case 5:
-            threeValuesCrescent()
+            n1 = int(input("Digite o primeiro número:\n=> "))
+            n2 = int(input("Digite o segundo número:\n=> "))
+            n3 = int(input("Digite o terceiro número:\n=> "))
+            Funcao.ordemCrescente(n1, n2, n3)
         case 6:
-            idealWeight()
+            altura = float(input("Digite sua altura:\n=> "))
+            sexo = int(input("Digite 1 para Feminino ou 2 para Masculino:\n=> "))
+            Funcao.pesoIdeal(altura, sexo)
         case 7:
-            regularPolygon()
+            numeroLados = int(input("Digite o número de lados:\n=> "))
+            cmLados = float(input("Digite quantos cm tem cada lado:\n=> "))
+            Funcao.poligono(numeroLados, cmLados)
         case 8:
-            regularPolygon()
+            valor1 = int(input("Digite o primeiro valor:\n=> "))
+            valor2 = int(input("Digite o segundo valor:\n=> "))
+            valor3 = int(input("Digite o terceiro valor:\n=> "))
+            Funcao.maiorDosTres(valor1, valor2, valor3)
         case 9:
-            threeValuesBiggest()
+            angulo1 = float(input("Digite o primeiro ângulo:\n=> "))
+            angulo2 = float(input("Digite o segundo ângulo:\n=> "))
+            angulo3 = float(input("Digite o terceiro ângulo:\n=> "))
+            Funcao.definicaoTriangulo(angulo1, angulo2, angulo3)
         case 10:
-            trianguleDefinition()
+            angulo1 = float(input("Digite o primeiro ângulo:\n=> "))
+            angulo2 = float(input("Digite o segundo ângulo:\n=> "))
+            angulo3 = float(input("Digite o terceiro ângulo:\n=> "))
+            Funcao.anguloTriangulo(angulo1, angulo2, angulo3)
         case 11:
-            trianguleAngule()
+            numero = int(input("Digite o ângulo que deseja obter o fatorial:\n=> "))
+            Funcao.fatorial(numero)
         case _:
-            print("Digite uma opcao existente")
+            print("Opção inválida.")
 
     os.system("pause")
